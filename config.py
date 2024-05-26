5f49cd884614685a22b2acdeb3eb2eb38193e89a
mattanapol <mattanapol.k@gmail.com>
2024-05-26 16:03:44 +0700

    Add langgraph and use multi step prompt

--- config.py
+++ config.py
@@ -9,10 +9,16 @@ class OpenAIConf:
     def __init__(self, raw):
         self.api_key = raw['api-key']
 
+class MyConf:
+    names: list[str]
+    def __init__(self, raw):
+        self.names = raw['names']
+
 class Config:
     def __init__(self, raw):
         self.slack = SlackConf(raw['slack'])
         self.open_ai = OpenAIConf(raw['open-ai'])
+        self.my = MyConf(raw['my'])
 
 def load_conf(path: str):
     with open(path, "r") as f:
